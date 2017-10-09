d3a78f53502b748daafb74234e097c91455f64e6
carlos <carlos@localhost>
2017-10-09 13:32:18 +0200

    optimized build script

--- build.sh
+++ build.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 VERSION="1.0.0-SNAPSHOT"
-GOOS=linux GOARCH=amd64 go build -o ./build/linux-amd64/thingpublisher -ldflags "-X main.Version=$VERSION" $1
-cp -r ./conf ./build/linux-amd64/
-sed -i "s/test.mosquitto.org/localhost/g" ./build/linux-amd64/conf/thing-publisher.json
-cp LICENSE ./build/linux-amd64/
-mkdir ./build/linux-amd64/dropzone
-mkdir ./build/linux-amd64/agents
-cd ./build/linux-amd64/ && tar -zcvf "ThingPublisher-linux-amd64-$VERSION.tar.gz" ./
-cd ../../
 
-GOOS=linux GOARCH=arm go build -o ./build/linux-arm/thingpublisher -ldflags "-X main.Version=$VERSION" $1
-cp -r ./conf ./build/linux-arm/
-sed -i "s/test.mosquitto.org/localhost/g" ./build/linux-arm/conf/thing-publisher.json
-cp LICENSE ./build/linux-arm/
-mkdir ./build/linux-arm/dropzone
-mkdir ./build/linux-arm/agents
-cd ./build/linux-arm/ && tar -zcvf "ThingPublisher-linux-arm-$VERSION.tar.gz" ./
-
-
+ARCH=( amd64 arm )
+for i in  "${ARCH[@]}"
+do
+  GOOS=linux GOARCH=$i go build -o ./build/linux-$i/thingpublisher -ldflags "-X main.Version=$VERSION" $1
+  cp -r ./conf ./build/linux-$i/
+  sed -i "s/test.mosquitto.org/localhost/g" ./build/linux-$i/conf/thing-publisher.json
+  cp LICENSE ./build/linux-$i/
+  mkdir ./build/linux-$i/dropzone
+  mkdir ./build/linux-$i/agents
+  cd ./build/linux-$i/ && tar -zcvf "ThingPublisher-linux-$i-$VERSION.tar.gz" ./
+  cd ../../
+done
