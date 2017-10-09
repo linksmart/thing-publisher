2eea01ffc6c6c61188adc9b967545563c8698f28
carlos <carlos@localhost>
2017-10-09 10:21:48 +0200

    test broker changed to localhost broker

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 VERSION="1.0.0-SNAPSHOT"
 GOOS=linux GOARCH=amd64 go build -o ./build/linux-amd64/thingpublisher -ldflags "-X main.Version=$VERSION" $1
 cp -r ./conf ./build/linux-amd64/
+sed -i "s/test.mosquitto.org/localhost/g" ./build/linux-amd64/thing-publisher.json
 cp LICENSE ./build/linux-amd64/
 mkdir ./build/linux-amd64/dropzone
 mkdir ./build/linux-amd64/agents
@@ -10,6 +11,7 @@ cd ../../
 
 GOOS=linux GOARCH=arm go build -o ./build/linux-arm/thingpublisher -ldflags "-X main.Version=$VERSION" $1
 cp -r ./conf ./build/linux-arm/
+sed -i "s/test.mosquitto.org/localhost/g" ./build/linux-arm/thing-publisher.json
 cp LICENSE ./build/linux-arm/
 mkdir ./build/linux-arm/dropzone
 mkdir ./build/linux-arm/agents
