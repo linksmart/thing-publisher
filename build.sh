19ede4c8594a2328af619cbc30ae165687479291
carlos <carlos@localhost>
2017-10-09 10:30:33 +0200

    fixed buildscript

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 VERSION="1.0.0-SNAPSHOT"
 GOOS=linux GOARCH=amd64 go build -o ./build/linux-amd64/thingpublisher -ldflags "-X main.Version=$VERSION" $1
 cp -r ./conf ./build/linux-amd64/
-sed -i "s/test.mosquitto.org/localhost/g" ./build/linux-amd64/thing-publisher.json
+sed -i "s/test.mosquitto.org/localhost/g" ./build/linux-amd64/conf/thing-publisher.json
 cp LICENSE ./build/linux-amd64/
 mkdir ./build/linux-amd64/dropzone
 mkdir ./build/linux-amd64/agents
@@ -11,7 +11,7 @@ cd ../../
 
 GOOS=linux GOARCH=arm go build -o ./build/linux-arm/thingpublisher -ldflags "-X main.Version=$VERSION" $1
 cp -r ./conf ./build/linux-arm/
-sed -i "s/test.mosquitto.org/localhost/g" ./build/linux-arm/thing-publisher.json
+sed -i "s/test.mosquitto.org/localhost/g" ./build/linux-arm/conf/thing-publisher.json
 cp LICENSE ./build/linux-arm/
 mkdir ./build/linux-arm/dropzone
 mkdir ./build/linux-arm/agents
