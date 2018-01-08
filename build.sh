df15379a7649f9c4cba59d2eb29194e4e60bcbd4
carlos <carlos@localhost>
2018-01-08 16:46:58 +0100

    post commit

--- build.sh
+++ build.sh
@@ -2,9 +2,10 @@
 VERSION="1.0.0-SNAPSHOT"
 
 ARCH=( amd64 arm )
+PLATFORM=linux
 for i in  "${ARCH[@]}"
 do
-  GOOS=linux GOARCH=$i go build -o ./build/linux-$i/thingpublisher -ldflags "-X main.Version=$VERSION" $1
+  GOOS=$PLATFORM GOARCH=$i go build -o ./build/linux-$i/thingpublisher -ldflags "-X main.Version=$VERSION" $1
   cp -r ./conf ./build/linux-$i/
   sed -i "s/iot.eclipse.org/localhost/g" ./build/linux-$i/conf/thing-publisher.json
   cp LICENSE ./build/linux-$i/
@@ -15,9 +16,9 @@ do
   sha256sum -b ThingPublisher-linux-$i-$VERSION.tar.gz > ThingPublisher-linux-$i-$VERSION.tar.gz.sha256
   SHA_OUT=$?
   if [ $SHA_OUT -eq 0 ];then
-    echo "linux/$i artifact build and check sum created, GOOD "
+    echo "$PLATFORM/$i artifact build and check sum created, GOOD "
   else
-    echo "Failed to create check sum for linux/$i. ERROR !"
+    echo "Failed to create check sum for $PLATFORM/$i. ERROR !"
     exit 1
 fi
   cd ../../
