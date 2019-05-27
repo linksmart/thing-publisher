2d5d4a65ff8324f2ca21385ca54ab43b9ec92dd9
Shreekantha Devasya <dschowta@users.noreply.github.com>
2019-05-27 17:12:45 +0200

    Changes in signal handling

--- main.go
+++ main.go
@@ -18,29 +18,16 @@ func main() {
 	log.Println("****************************************")
 	log.Println("Linksmart ThingPublisher v", Version)
 	log.Println("****************************************")
-
-	c := make(chan os.Signal, 2)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
-	go func() {
-		<-c
-		fmt.Println("C-CTRL detected")
-		os.Exit(1)
-	}()
-
-
+	fmt.Println("Press ctrl-C to exit")
+	
 	am := newAgentManager()
 	go am.startAgentManager()
 	defer am.stopAgentManager()
-
-
-
-
-
-	for {
-			//fmt.Println("LSTP  alive...")
-			time.Sleep(10 * time.Second)
-	}
-
+	
+	c := make(chan os.Signal, 2)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	<-c
+	fmt.Println("Stopping")
 }
 
 
